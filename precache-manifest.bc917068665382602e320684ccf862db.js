5ff13ac5e65226573ed3adf625a9ccc9b64b3671
mohamedessamkamal50 <mohamedessamkamal50@gmail.com>
2020-10-31 16:28:48 +0200

    Updates

---
+++ precache-manifest.bc917068665382602e320684ccf862db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e437101374592d343f9c4fd85758e2a",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "ce16f453a83f07b0371b",
+    "url": "/slider/static/css/main.06911859.chunk.css"
+  },
+  {
+    "revision": "ae235d1c30287f6de9a4",
+    "url": "/slider/static/js/2.5380b2ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/slider/static/js/2.5380b2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce16f453a83f07b0371b",
+    "url": "/slider/static/js/main.3d32e50c.chunk.js"
+  },
+  {
+    "revision": "6e71670bba41fe38bf99",
+    "url": "/slider/static/js/runtime-main.ac01f132.js"
+  }
+]);
